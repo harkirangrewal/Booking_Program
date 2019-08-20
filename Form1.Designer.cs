a1def6527c40d2982ace101db7f4ea7c6700e7b9
Harkiran Grewal <40836336+harkirangrewal@users.noreply.github.com>
2019-08-20 15:29:08 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Theatre_Booking_Brook_Harkiran
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblRow = new System.Windows.Forms.Label();
+            this.lblSeat = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.nudRow = new System.Windows.Forms.NumericUpDown();
+            this.nudSeat = new System.Windows.Forms.NumericUpDown();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lbSeatOutput = new System.Windows.Forms.ListBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnWho = new System.Windows.Forms.Button();
+            this.btnWhere = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSeat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(23, 56);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(43, 23);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(51, 20);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblRow
+            // 
+            this.lblRow.AutoSize = true;
+            this.lblRow.Location = new System.Drawing.Point(182, 23);
+            this.lblRow.Name = "lblRow";
+            this.lblRow.Size = new System.Drawing.Size(41, 20);
+            this.lblRow.TabIndex = 3;
+            this.lblRow.Text = "Row";
+            // 
+            // lblSeat
+            // 
+            this.lblSeat.AutoSize = true;
+            this.lblSeat.Location = new System.Drawing.Point(275, 23);
+            this.lblSeat.Name = "lblSeat";
+            this.lblSeat.Size = new System.Drawing.Size(56, 20);
+            this.lblSeat.TabIndex = 5;
+            this.lblSeat.Text = "Seat #";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(135, 102);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(129, 46);
+            this.btnSelect.TabIndex = 6;
+            this.btnSelect.Text = "Purchase Seat";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // nudRow
+            // 
+            this.nudRow.Location = new System.Drawing.Point(176, 57);
+            this.nudRow.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudRow.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudRow.Name = "nudRow";
+            this.nudRow.ReadOnly = true;
+            this.nudRow.Size = new System.Drawing.Size(76, 26);
+            this.nudRow.TabIndex = 7;
+            this.nudRow.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudSeat
+            // 
+            this.nudSeat.Location = new System.Drawing.Point(279, 57);
+            this.nudSeat.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.nudSeat.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudSeat.Name = "nudSeat";
+            this.nudSeat.ReadOnly = true;
+            this.nudSeat.Size = new System.Drawing.Size(64, 26);
+            this.nudSeat.TabIndex = 8;
+            this.nudSeat.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(218, 169);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(100, 26);
+            this.txtTotal.TabIndex = 9;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(28, 172);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(129, 20);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total Price (HST)";
+            // 
+            // lbSeatOutput
+            // 
+            this.lbSeatOutput.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSeatOutput.FormattingEnabled = true;
+            this.lbSeatOutput.ItemHeight = 23;
+            this.lbSeatOutput.Location = new System.Drawing.Point(83, 350);
+            this.lbSeatOutput.Name = "lbSeatOutput";
+            this.lbSeatOutput.Size = new System.Drawing.Size(260, 142);
+            this.lbSeatOutput.TabIndex = 11;
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(23, 209);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(119, 56);
+            this.btnList.TabIndex = 12;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(74, 283);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(119, 56);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Cancel Seat";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnWho
+            // 
+            this.btnWho.Location = new System.Drawing.Point(159, 209);
+            this.btnWho.Name = "btnWho";
+            this.btnWho.Size = new System.Drawing.Size(119, 56);
+            this.btnWho.TabIndex = 14;
+            this.btnWho.Text = "Who";
+            this.btnWho.UseVisualStyleBackColor = true;
+            this.btnWho.Click += new System.EventHandler(this.btnWho_Click);
+            // 
+            // btnWhere
+            // 
+            this.btnWhere.Location = new System.Drawing.Point(297, 209);
+            this.btnWhere.Name = "btnWhere";
+            this.btnWhere.Size = new System.Drawing.Size(119, 56);
+            this.btnWhere.TabIndex = 15;
+            this.btnWhere.Text = "Where";
+            this.btnWhere.UseVisualStyleBackColor = true;
+            this.btnWhere.Click += new System.EventHandler(this.btnWhere_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(224, 283);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(119, 56);
+            this.btnTotal.TabIndex = 16;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 504);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnWhere);
+            this.Controls.Add(this.btnWho);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.lbSeatOutput);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.nudSeat);
+            this.Controls.Add(this.nudRow);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lblSeat);
+            this.Controls.Add(this.lblRow);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Ticket Booking";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudRow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSeat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblRow;
+        private System.Windows.Forms.Label lblSeat;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.NumericUpDown nudRow;
+        private System.Windows.Forms.NumericUpDown nudSeat;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.ListBox lbSeatOutput;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnWho;
+        private System.Windows.Forms.Button btnWhere;
+        private System.Windows.Forms.Button btnTotal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Theatre_Booking_Brook_Harkiran
+{
+    public partial class Form1 : Form
+    {
+        public string[,] seatArray = new string[5, 9];
+
+        public Form1()
+        {
+            int row, col;
+            InitializeComponent();
+
+            for (row = 0; row < 5; row = row + 1)
+            {
+                for (col = 0; col < 9; col = col + 1)
+                {
+                    seatArray[row, col] = "";
+                }
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            int iRow, iSeat;
+            string strName;
+            double dTotal;
+
+
+
+            strName = Convert.ToString(txtName.Text);
+
+            iRow = Convert.ToInt32(nudRow.Value);
+            iSeat = Convert.ToInt32(nudSeat.Value);
+
+            strName = strName.Trim();
+            strName = strName.ToLower();
+
+            try
+            {
+                if (strName == "")
+                {
+                    MessageBox.Show("Please enter a name for the desired seat");
+                }
+                // Given values from user input are saved
+                // The program errors if there is no name inputted from the user
+
+                iRow = iRow - 1;
+                iSeat = iSeat - 1;
+
+                if (seatArray[iRow, iSeat] == "")
+                {
+                    seatArray[iRow, iSeat] = strName;
+                    txtName.Clear();
+
+                    if (iRow < 2 && (iSeat > 3 && iSeat < 5))
+                    {
+                        dTotal = 40;
+                    }
+
+                    else if (iRow == 2 && (iSeat > 3 && iSeat < 5))
+                    {
+                        dTotal = 30;
+                    }
+                    else if ((iRow < 5 && iRow > 2) && (iSeat > 3 && iSeat < 5))
+                    {
+                        dTotal = 20;
+                    }
+
+                    else if (iRow < 2 && (iSeat < 3 || iSeat > 5))
+                    {
+                        dTotal = 35;
+                    }
+                    else if (iRow == 2 && (iSeat < 3 || iSeat > 5))
+                    {
+                        dTotal = 25;
+                    }
+                    else
+                    {
+                        dTotal = 10;
+                    }
+
+                    dTotal = dTotal * 1.13;
+                    txtTotal.Text = Convert.ToString(dTotal);
+                }
+                // Checks if the user's resired spot is empty
+                //If it is, a price is given for a specific row and seat chosen. 
+                // The price is then outputted (along with tax), in a textbox. 
+
+                else
+                {
+                    MessageBox.Show("This seat is already taken");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("This seat does not exist.");
+                return;
+            }
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            string strLineOutput;
+
+            try
+            {
+                lbSeatOutput.Items.Clear();
+                for (i = 0; i < 5; i = i + 1)
+                {
+                    strLineOutput = "";
+
+                    for (j = 0; j < 9; j = j + 1)
+                    {
+                        if (seatArray[i, j] == "")
+                        {
+                            strLineOutput = strLineOutput + "O";
+                        }
+                        else
+                        {
+                            strLineOutput = strLineOutput + "X";
+                        }
+                    }
+
+                    lbSeatOutput.Items.Add(strLineOutput);
+                }
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Could not display seats.");
+                return;
+            }
+
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            int iRow, iCol;
+            string strName;
+
+            lbSeatOutput.Items.Clear();
+
+            strName = Convert.ToString(txtName.Text);
+            iRow = Convert.ToInt32(nudRow.Value);
+            iCol = Convert.ToInt32(nudSeat.Value);
+
+            try
+            {
+                // Sit 2: We are given the values, but the seat is empty
+                if (strName == "")
+                {
+                    MessageBox.Show("Please enter a name to cancel.");
+                }
+
+                // Situtation 1: We are given a seat and name, to cancel
+                if (strName == seatArray[iRow, iCol])
+                {
+                    seatArray[iRow, iCol] = "";
+                    MessageBox.Show("Cancel complete.");
+                    return;
+                }
+
+                // Sit 3. The inputted name does not match the name in the array 
+                else
+                {
+                    MessageBox.Show("This seat does not belong to you.");
+                    return;
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Please enter valid input.");
+                return;
+            }
+        }
+
+        private void btnWho_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int iRow, iCol;
+                string strName;
+
+                txtName.Clear();
+
+                lbSeatOutput.Items.Clear();
+                iRow = Convert.ToInt32(nudRow.Value);
+                iCol = Convert.ToInt32(nudSeat.Value);
+                // Gets input values from user.
+
+                strName = seatArray[iRow - 1, iCol - 1];
+
+                if (strName == "")
+                {
+                    MessageBox.Show("Sorry, nobody is sitting in that spot");
+                }
+                else
+                {
+                    MessageBox.Show("This seat belongs to " + strName);
+                }
+                // Tells the user whether someone is sitting in the entered seat values.
+            }
+            catch
+            {
+                MessageBox.Show("Error, that is an invalid input");
+                return;
+            }
+        }
+
+        private void btnWhere_Click(object sender, EventArgs e)
+        {
+            string strName;
+            int i;
+
+            lbSeatOutput.Items.Clear();
+            strName = Convert.ToString(txtName.Text);
+            try
+            {
+
+                for (i = 0; i < 5; i = i + 1)
+                {
+                    for (int j = 0; j < 9; j = j + 1)
+                    {
+                        if (strName == seatArray[i, j])
+                        {
+                            i = i + 1;
+                            j = j + 1;
+                            MessageBox.Show(strName + " is sitting in row: " + i + " seat " + j);
+                            return;
+                        }
+                       
+                    }
+                }
+
+                MessageBox.Show("Sorry, this person has not bought a seat");
+                return;
+            }
+
+            catch
+            {
+                MessageBox.Show("Error, invalid entry");
+                return;
+            }
+
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            int i, j, iBookedSeats, iEmptySeats;
+
+            iEmptySeats = 0;
+            iBookedSeats = 0;
+
+            try
+            {
+                txtName.Clear();
+                lbSeatOutput.Items.Clear();
+
+                for (i = 0; i < 5; i = i + 1)
+                {
+                    for (j = 0; j < 9; j = j + 1)
+                    {
+                        if (seatArray[i, j] == "")
+                        {
+                            iEmptySeats = iEmptySeats + 1;
+                        }
+                        else
+                        {
+                            iBookedSeats = iBookedSeats + 1;
+                        }
+                    }
+
+
+
+                }
+                MessageBox.Show("There are " + iEmptySeats + " empty seats, and " + iBookedSeats + " booked seats.");
+               
+            }
+
+            catch
+            {
+                MessageBox.Show("Invalid input, please try again");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
